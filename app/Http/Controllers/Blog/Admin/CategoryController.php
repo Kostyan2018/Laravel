a0cc695758235ffb247f08d15fbe01988e642eec
Kostyan2018 <kopihb71@gmail.com>
2020-03-25 11:58:41 +0200

    11

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Blog\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CategoryContrller extends Controller
+class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
