be6d9a4ed1ff31ee32241fdf10b30a0e6f26d761
Kostyan2018 <kopihb71@gmail.com>
2020-04-07 23:10:54 +0300

    Вівторок 07  квітня

--- resources/views/blog/admin/category/edit.blade.php
+++ resources/views/blog/admin/category/edit.blade.php
@@ -11,10 +11,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                @include('blog.category.includes.item_edit_main_col')
+                @include('blog.admin.category.includes.item_edit_main_col')
             </div>
             <div class="col-md-3">
-                @include('blog.category.includes.item_edit_add_col')
+                @include('blog.admin.category.includes.item_edit_add_col')
             </div>
         </div>
     </div>
--- resources/views/blog/admin/category/includes/item_edit_main_col.blade.php
+++ resources/views/blog/admin/category/includes/item_edit_main_col.blade.php
@@ -1,5 +1,6 @@
 @php
 /** @var \App\Models\BloCategory $item */
+/** @var \App\Models\BloCategory $categoryList*/
 @endphp
 
 <div class="row justify-content-center">
@@ -11,8 +12,56 @@
                       <a class="nav-link active" data-toggle="tab" href="#maindata" role="tab">Main Data</a>
                     </li>
                 </ul>
+                <br>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="maindata" role="tabpanel">
+                        <div class="form-group">
+                            <label for="title">Заголовок</label>
+                        <input name="title" value="{{ $item->title }}"
+                        id="title" 
+                        type="text"
+                        class="form-control"
+                        minlength="3"
+                        required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="slug">Идентифікатор</label>
+                        <input name="slug" value="{{ $item->slug }}"
+                        id="slug" 
+                        type="text"
+                        class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                        <label for="parent_id">Батьківська категорія</label>
+                        <select name="parent_id" 
+                        id="parent_id" 
+                        class="form-control"
+                        placeholder="Вибиріть категорію"
+                        required>
+                            @foreach ($categoryList as $categoryOption)
+                                <option value="{{ $categoryOption->id }}"
+                                    @if ($categoryOption->id == $item->parent_id) selected @endif>
+                                    {{ $categoryOption->id }}. {{ $categoryOption->title }}
+                                </option>                            
+                            @endforeach
+                        </select>
+                        </div>
+
+                        <div class="form-group">
+                        <label for="description">Описание</label>
+
+                        <textarea name="description" style="resize: none"  
+                        id="description" 
+                        class="form-control"
+                        rows="4">{{ $item->slug }}</textarea>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<br>
+
